83acb6b4437d87360ba275960dd2ff8d2fffafc5
Aman choudhary <112798604+amanchoudhary7@users.noreply.github.com>
2024-01-11 20:59:55 +0530

    Update TextEditor.java

--- Java-Text-editor/TextEditor.java
+++ Java-Text-editor/TextEditor.java
@@ -23,7 +23,7 @@ public class TextEditor extends JFrame implements ActionListener{
 
  TextEditor(){
   this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  this.setTitle("Bro text Editor");
+  this.setTitle("Aman text Editor");
   this.setSize(500, 500);
   this.setLayout(new FlowLayout());
   this.setLocationRelativeTo(null);
